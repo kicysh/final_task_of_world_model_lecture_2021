bfae9c0b2542e6ce16e19ebf882c0d53ee548d09
kicysh <71452166+kicysh@users.noreply.github.com>
2022-04-05 00:04:55 +0900

    Create setup.py

---
+++ package/setup.py
@@ -0,0 +1,10 @@
+import setuptools import setup,find_packages
+
+setup(
+	name='LDVAE',
+	version='0.0.0',
+	description='LDVAE model',
+	author='kicysh',
+	install_requires=['pytorch'],
+	packages=['ldvae'],
+)
